b089e539054882d9fb290013c2d23eb001792174
woothemes <woothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-06 07:24:50 +0000

    WooAffiliates V1.0.0 - First release. Woo!
    git-svn-id: https://plugins.svn.wordpress.org/wooaffiliates/trunk@471456 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woo-affiliates.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: WooThemes Affiliate Widget
+Plugin URI: http://woothemes.com/
+Description: Easily display your WooThemes affiliate links in a widget or shortcode.
+Version: 1.0.0
+Author: Matty at WooThemes
+Author URI: http://woothemes.com/
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+/*  Copyright 2011  WooThemes  (email : info@woothemes.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+	require_once( 'classes/affiliates.class.php' );
+	
+	global $woo_affiliates;
+	
+	$woo_affiliates = new Woo_Affiliates();
+?>
